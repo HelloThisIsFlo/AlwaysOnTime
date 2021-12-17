4f36173d29c22c4bd80e917d16d6be262264bb88
Florian Kempenich <Flori@nKempenich.com>
2021-12-17 11:04:46 +0000

    Display oauth token

--- alwaysontime/timers/views.py
+++ alwaysontime/timers/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from allauth.socialaccount.models import SocialToken
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 @login_required
 def index(request):
-    return render(request, template_name='index.html')
+    access_token = SocialToken.objects.get(account__user=request.user)
+
+    return render(request, 'index.html', {
+        'token': access_token
+    })
