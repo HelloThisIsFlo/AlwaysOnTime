f408226b5641e13b1a2f5107aa128d7233473e17
Florian Kempenich <Flori@nKempenich.com>
2021-12-22 10:13:24 +0000

    Refresh credentials & build service

--- alwaysontime/timers/google_api.py
+++ alwaysontime/timers/google_api.py
@@ -1,13 +1,15 @@
 from allauth.socialaccount.models import SocialApp
 from django.conf import settings
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 
 
 class GoogleCalendarApi:
     def __init__(self, token, refresh_token):
         google_app = SocialApp.objects.get(name=settings.GOOGLE_APP_NAME)
 
-        Credentials(
+        creds = Credentials(
                 token=token,
                 refresh_token=refresh_token,
                 token_uri='https://oauth2.googleapis.com/token',
@@ -15,6 +17,9 @@ class GoogleCalendarApi:
                 client_secret=google_app.secret,
                 scopes=settings.GOOGLE_SCOPES
         )
+        if creds.expired:
+            creds.refresh(Request())
+        calendar_service = build('calendar', 'v3', credentials=creds)
 
     def events(self, calendar_id, before, after, order_by):
         pass
--- alwaysontime/timers/tests/test_google_api.py
+++ alwaysontime/timers/tests/test_google_api.py
@@ -4,22 +4,24 @@ import pytest
 
 import timers.google_api
 from alwaysontime.settings import GOOGLE_SCOPES
-from conftest import TEST_GOOGLE_TOKEN, TEST_GOOGLE_REFRESH_TOKEN, \
-    TEST_GOOGLE_APP_CLIENT_ID, TEST_GOOGLE_APP_SECRET
-from timers.calendar import refresh_all_events
+from conftest import TEST_GOOGLE_APP_CLIENT_ID, TEST_GOOGLE_APP_SECRET
 from timers.google_api import GoogleCalendarApi
 
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture
+def CredentialsMock():
+    with patch.object(timers.google_api, 'Credentials') as CredentialsMock:
+        CredentialsMock().expired = False
+        CredentialsMock.reset_mock()
+        yield CredentialsMock
+
+
 class TestInit:
-    @patch.object(timers.google_api, 'Credentials')
     def test_create_credentials_from_user_token_using_google_app(
             self, CredentialsMock, test_user
     ):
-        CredentialsMock().expired = False
-        CredentialsMock.reset_mock()
-
         token = 'token'
         refresh_token = 'refresh_token'
 
@@ -34,14 +36,36 @@ class TestInit:
                 scopes=GOOGLE_SCOPES
         )
 
-    def test_refresh_credentials_if_expired(self):
-        pass
+    @patch.object(timers.google_api, 'Request')
+    def test_refresh_credentials_if_expired(
+            self, RequestMock, CredentialsMock, test_user
+    ):
+        credentials_mock = CredentialsMock()
+        credentials_mock.expired = True
 
-    def test_dont_refresh_credentials_if_not_expired(self):
-        pass
+        GoogleCalendarApi('not_used', 'not_used')
 
-    def test_build_calendar_service_with_credentials(self):
-        pass
+        credentials_mock.refresh.assert_called_once_with(RequestMock())
+
+    def test_dont_refresh_credentials_if_not_expired(
+            self, CredentialsMock, test_user
+    ):
+        credentials_mock = CredentialsMock()
+        credentials_mock.expired = False
+
+        GoogleCalendarApi('not_used', 'not_used')
+
+        credentials_mock.refresh.assert_not_called()
+
+    @patch.object(timers.google_api, 'build')
+    def test_build_calendar_service_with_credentials(
+            self, build_mock, CredentialsMock, test_user
+    ):
+        credentials_mock = CredentialsMock()
+        GoogleCalendarApi('not_used', 'not_used')
+        build_mock.assert_called_once_with('calendar',
+                                           'v3',
+                                           credentials=credentials_mock)
 
 
 class TestEvents:
@@ -70,80 +94,3 @@ class TestEvents:
     def test_raise_error_if_request_failed(self):
         # Not sure how to test that. We'll see
         pass
-
-
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-## TODO: This needs to be migrated still!! ##
-
-@pytest.mark.skip
-@patch.object(timers.calendar, 'Credentials')
-def test_create_credentials_from_user_token(CredentialsMock,
-                                            test_user):
-    CredentialsMock().expired = False
-    CredentialsMock.reset_mock()
-
-    refresh_all_events(test_user)
-
-    CredentialsMock.assert_called_once_with(
-            token=TEST_GOOGLE_TOKEN,
-            refresh_token=TEST_GOOGLE_REFRESH_TOKEN,
-            token_uri='https://oauth2.googleapis.com/token',
-            client_id=TEST_GOOGLE_APP_CLIENT_ID,
-            client_secret=TEST_GOOGLE_APP_SECRET,
-            scopes=GOOGLE_SCOPES
-    )
-
-
-@pytest.mark.skip
-@patch.object(timers.calendar, 'Request')
-@patch.object(timers.calendar, 'Credentials')
-def test_refresh_credentials_if_expired(CredentialsMock,
-                                        RequestMock,
-                                        test_user):
-    credentials_mock = CredentialsMock()
-    credentials_mock.expired = True
-    refresh_all_events(test_user)
-    credentials_mock.refresh.assert_called_once_with(RequestMock())
-
-
-@pytest.mark.skip
-@patch.object(timers.calendar, 'Credentials')
-def test_dont_refresh_credentials_if_not_expired(CredentialsMock,
-                                                 test_user):
-    credentials_mock = CredentialsMock()
-    credentials_mock.expired = False
-    refresh_all_events(test_user)
-    credentials_mock.refresh.assert_not_called()
-
-
-@pytest.mark.skip
-@patch.object(timers.calendar, 'build')
-@patch.object(timers.calendar, 'Credentials')
-def test_build_calendar_service_with_credentials(CredentialsMock,
-                                                 build_mock,
-                                                 test_user):
-    credentials_mock = CredentialsMock()
-    credentials_mock.expired = False
-    refresh_all_events(test_user)
-    build_mock.assert_called_once_with('calendar',
-                                       'v3',
-                                       credentials=credentials_mock)
